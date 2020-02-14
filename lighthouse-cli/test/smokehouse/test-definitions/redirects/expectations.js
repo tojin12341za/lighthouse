f9f102cbad8a3a5976c2e8319c2c99e6d4d64227
Patrick Hulce <patrick.hulce@gmail.com>
2020-02-13 22:15:34 -0600

    remove the redirects portion

--- lighthouse-cli/test/smokehouse/test-definitions/redirects/expectations.js
+++ lighthouse-cli/test/smokehouse/test-definitions/redirects/expectations.js
@@ -16,18 +16,8 @@ const expectations = [
       requestedUrl: `http://localhost:10200/online-only.html?delay=2000&redirect=%2Fredirects-final.html`,
       finalUrl: 'http://localhost:10200/redirects-final.html',
       audits: {
-        'first-contentful-paint': {
-          numericValue: '>=2000',
-        },
-        'interactive': {
-          numericValue: '>=2000',
-        },
-        'speed-index': {
-          numericValue: '>=2000',
-        },
         'redirects': {
           score: 1,
-          numericValue: '>=2000',
           details: {
             items: {
               length: 2,
@@ -46,18 +36,8 @@ const expectations = [
       requestedUrl: `http://localhost:10200/online-only.html?delay=1000&redirect_count=3&redirect=%2Fredirects-final.html`,
       finalUrl: 'http://localhost:10200/redirects-final.html',
       audits: {
-        'first-contentful-paint': {
-          numericValue: '>=3000',
-        },
-        'interactive': {
-          numericValue: '>=3000',
-        },
-        'speed-index': {
-          numericValue: '>=3000',
-        },
         'redirects': {
           score: '<1',
-          numericValue: '>=3000',
           details: {
             items: {
               length: 4,
@@ -77,15 +57,6 @@ const expectations = [
       requestedUrl: `http://localhost:10200/js-redirect.html?delay=2000&jsDelay=5000&jsRedirect=%2Fredirects-final.html`,
       finalUrl: 'http://localhost:10200/redirects-final.html',
       audits: {
-        'redirects': {
-          score: 1,
-          numericValue: '>=7000',
-          details: {
-            items: {
-              length: 2,
-            },
-          },
-        },
       },
       runWarnings: [
         /The page may not be loading as expected because your test URL \(.*js-redirect.html.*\) was redirected to .*redirects-final.html. Try testing the second URL directly./,
@@ -99,25 +70,6 @@ const expectations = [
       requestedUrl: `http://localhost:10200/js-redirect.html?delay=2000&jsDelay=5000&jsRedirect=%2Fonline-only.html%3Fdelay%3D1000%26redirect%3D%2Fredirects-final.html`,
       finalUrl: 'http://localhost:10200/redirects-final.html',
       audits: {
-        // Just captures the server-side at the moment, should be 8s in the future
-        'first-contentful-paint': {
-          numericValue: '>=1000',
-        },
-        'interactive': {
-          numericValue: '>=1000',
-        },
-        'speed-index': {
-          numericValue: '>=1000',
-        },
-        'redirects': {
-          score: '<1',
-          numericValue: '>=8000',
-          details: {
-            items: {
-              length: 3,
-            },
-          },
-        },
       },
       runWarnings: [
         /The page may not be loading as expected because your test URL \(.*js-redirect.html.*\) was redirected to .*redirects-final.html. Try testing the second URL directly./,
